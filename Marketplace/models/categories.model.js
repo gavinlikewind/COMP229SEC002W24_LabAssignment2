fb1fb0a6df1badbac977aedf64e0bac25117f6e6
gavinlikewind <gavinlikewind@gmail.com>
2024-02-18 16:51:33 -0500

    remove useless file

--- Marketplace/models/categories.model.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require("mongoose");
-const categoriesSchema = mongoose.Schema({
-    name:{type: String, trim: true, required: "Category Name is required."}
-});
-
-module.exports = mongoose.model("categories", categoriesSchema);
